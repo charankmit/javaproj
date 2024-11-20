adbb1613c0f65787f8d34c07a18624f61cfb5656
charankmit <charankmit@gmail.com>
2024-11-20 15:27:59 +0530

    v3

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 class Main
 {
     public static void main(String args[]){
-        int x=(int)args[0];
-        if(x/2==0)
+        int x=Integer.parseInt(args[0]);
+        if(x%2 == 0){
         System.out.println(x+" is a Even number");
+        }
         else
         System.out.println(x+" is a Odd number");
     }

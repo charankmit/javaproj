c2451608cc0837f7c8d4424967b58a84c02e5b66
charankmit <charankmit@gmail.com>
2024-11-20 15:11:37 +0530

    v1

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main
+{
+    public static void main(String args[]){
+        if(args[0]/2==0)
+        System.out.println(args[0]+" is a Even number");
+        else
+        System.out.println(args[0]+" is a Odd number");
+    }
+}
